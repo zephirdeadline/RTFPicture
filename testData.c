a1a84933d97350e899e356bd8cf94e6e3a4e1249
troineullul <lucas.dessert@gmail.com>
2015-10-04 12:25:04 +0200

    commit de lucas correction de certaine erreur avec melvyn

--- testData.c
+++ testData.c
@@ -2,19 +2,26 @@
 #include <stdio.h>
 #include "data.h"
 
+
+
+
+
 int main()
 {
 	CharacterInfo *c = NULL;
-	c = ArrayRec(CounterData());
-	//c = AddInfo(c, "Anais");
-	//c = AddInfo(c, "Kris");
+	c = ArrayData();
+	//c = AddInfoStart(c, "Anais");
+	//c = AddInfoStart(c, "paul");
+	//c = AddInfoStart(c, "lucas");
 	//ModifInfo(c, "b", "hihi");
 	printf("Il y a %d elements\n", CountList(c));
-	c = RemoveInfo(c, "Anais");
-	//printf("Ily a %s elements\n", c->next->name);
+	//c = RemoveInfo(c, "e");
+	//char *res = Concat("aaa", "bbb");
+	//printf("Ily a %s elements\n", res);
 
 	ShowList(c);
-
+	//ShowList(c);
+	printf(" \n   %s   \n", c->next->name);
 	//ArrayData();
 	//AddInfo("ii", "oo", "oo");
 	//char*a;
