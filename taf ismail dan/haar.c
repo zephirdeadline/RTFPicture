6463d6828de0650f7aa0cc0bc093125f34fd2713
Dan Azoulay <daniel.azoulay@epita.fr>
2015-11-01 22:35:16 +0100

    le genie

--- taf ismail dan/haar.c
+++ taf ismail dan/haar.c
@@ -2,11 +2,116 @@
 # include <stdlib.h>
 # include <SDL/SDL.h>
 # include <SDL/SDL_image.h>
+#include "funcInter.h"
+#include "data.h"
 # include "haar.h"
 # include <math.h>
 # include <dirent.h>
 # include <string.h>
+int main (int argc, char *argv[])
+{
+  if(argc < 2)
+  {
+    printf("manque arg");
+  }
+  else
+  {
+    if(argc == 3)
+    {
+	  //deux argument:= 1er grey pour la fonction et le deuxième un nom d'image :appeler
+        if((strcmp(argv[1],"grey"))==0)
+        {
+	printf("fuck");
+          SDL_Surface *surface=IMG_Load(argv[2]);
+          goToGrey(surface);
+        }
+   // 2 argument:= 1er integral pour la fonction et le deuxième un nom d'image:appeler
+        if((strcmp(argv[1],"integral"))==0)
+        {
+          SDL_Surface *surface=IMG_Load(argv[2]);
+          Integral(surface);
+        }
+//2 argument:= 1er haar pour la fonction et le deuxième un nom d'image:appeler
+        if((strcmp(argv[1],"haar"))==0)
+        {
+          SDL_Surface *surface=IMG_Load(argv[2]);
+          haarr2(surface);
+        }
 
+//2 argument 1er decision 2eme entier i: appeler
+         if((strcmp(argv[1],"decision"))==0)
+        {
+          example* arr;
+	      image *tab = malloc(797*sizeof(image));
+         arr=weightImage(tab, 797, 797);	
+	      float *t=decision(arr,(atoi(argv[2]))/*i*/,797);
+        }
+//2 argument adaboost pour la fonction entier T pour le nombre de tour:appeler
+      if((strcmp(argv[1],"adaboost"))==0)
+      {
+        example* arr;
+	    image *tab = malloc(797*sizeof(image));
+	    arr=weightImage(tab, 797, 797);	
+	    adaboost(arr,atoi(argv[2]),797);
+      }
+    if(argc == 2)
+    {
+      //1 argument 1er image pour la fonction:appeler
+        if((strcmp(argv[1],"image"))==0)
+        {
+          example* arr;
+	      image *tab = malloc(797*sizeof(image));
+	      arr=weightImage(tab, 797, 797);
+        }
+
+    }
+      /*if(argc > 2)
+      {
+        if(argv[1] == "bd")
+        {
+          char *a = "jerome", *b = "guillaume", *c = "valentin" = *b = NULL;
+          char **tab = malloc(4 * sizeof(a));
+          tab[0] = a;
+          tab[1] = b;
+          tab[2] = c;
+          tab[3] = d;
+          CharacterInfo *c = NULL;
+          c= ArrayData();
+
+          if(argv[2] == "-add")
+          {
+            c=AddInfo(c, argv[3]);
+          }
+
+          if(argv[2] == "-addBatch")
+          {
+            c=AddBatch(c, tab);
+          }
+
+          if(argv[2] == "-remove")
+          {
+            c=RemoveInfo(c, argv[3]);
+          }
+
+           if(argv[2] == "-modif")
+          {
+            ModifInfo(c, argv[3], argv[4]);
+          }
+
+          if(argv[2] == "-ls")
+          {
+            ShowList(c);
+          }
+        }
+      }
+      else
+    {
+      printf("erreur arg");
+    }*/
+  }
+  }
+  return 0;
+}
 
 example* weightImage(image tab[], int len, int nb) 
 {	
