da73bedb45dda14a6b2e6feb520040b5b53e324e
Dan Azoulay <daniel.azoulay@epita.fr>
2015-11-01 18:41:18 +0100

    main

---
+++ taf ismail dan/main.c
@@ -0,0 +1,29 @@
+int main (int argc, char *argv[])
+{
+	/* deux argument:= 1er grey pour la fonction et le deuxième un nom d'image :appeler
+SDL_Surface surface=IMG_Load("nomdel'image");
+ goToGrey(surface)*/
+/* 2 argument:= 1er integral pour la fonction et le deuxième un nom d'image:appeler
+SDL_Surface surface=IMG_Load("nomdel'image");
+Integral(surface);*/
+/*2 argument:= 1er haar pour la fonction et le deuxième un nom d'image:appeler
+SDL_Surface surface=IMG_Load("nomdel'image");
+haarr2(surface);*/
+/*1 argument 1er image pour la fonction:appeler
+example* arr;
+	image *tab = malloc(797*sizeof(image));
+	
+	arr=weightImage(tab, 797, 797);	*/
+/*2 argument 1er decision 2eme entier i: appeler
+ example* arr;
+	image *tab = malloc(797*sizeof(image));
+	
+	arr=weightImage(tab, 797, 797);	
+	float *t=decision(arr,j,797);*/
+/*2 argument adaboost pour la fonction entier T pour le nombre de tour:appeler
+ example* arr;
+	image *tab = malloc(797*sizeof(image));
+	
+	arr=weightImage(tab, 797, 797);	
+	adaboost(arr,T,797);*/
+}
