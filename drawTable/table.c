d8e989f819e41006c22ea773c6c3f44915650489
w4pity <w4pity@gmail.com>
2015-12-06 01:18:43 +0100

    ajuste draw

--- drawTable/table.c
+++ drawTable/table.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 #define ZEROX = 640/2
 #define ZEROY = 480/2
 #include <time.h>
 #include <unistd.h>
-#include <SDL/SDL_ttf.h>
+//#include <SDL/SDL_ttf.h>
 
 struct vector
 {
@@ -27,79 +28,125 @@ void drawModulo(SDL_Surface *s, int t, int u);
 void findEq(SDL_Surface *s, int zerox, int zeroy, int r, float t);
 struct vector *findVal(int v);
 struct vector* tab[1000];
-
-int mod =200;
-float table = 2;
+float pow2(float a, int b);
+int mod =10;
+float table = 435;
+int max = 1;
+int X = 800;
+int Y = 600;
+char *type;
+float v = 1;
+int vmod = 0;
+int rd = 0, gd=0, bd=0, rf = 255, bf = 255, gf = 255;
 //int r = 200;
 int main(int argc, char *argv[])
 {
+  type = argv[1];
+  table = atoi(argv[2]);
+  max = atoi(argv[3]);
+  mod = atoi(argv[4]);
+  v = atof(argv[5]);
+  //vmod = atoi(argv[6]);
   SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
-  TTF_Init();
-  TTF_Font *police = TTF_OpenFont("DejaVuSans.ttf", 65);
-  SDL_Color couleurNoire = {255, 255, 255};
-  SDL_Color couleurNoire1 = {0, 0, 0};
+  //TTF_Init();
+  //TTF_Font *police = TTF_OpenFont("DejaVuSans.ttf", 65);
+  //SDL_Color couleurNoire = {255, 255, 255};
+  //SDL_Color couleurNoire1 = {0, 0, 0};
 
-  int zerox = 640/2, zeroy = 480/2;
+  int zerox = X/2, zeroy = Y/2;
   SDL_Surface *ecran = NULL, *texte = NULL;
-  ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+  ecran = SDL_SetVideoMode(X, Y, 32, SDL_HWSURFACE);
   SDL_Rect position;
   position.x = 0;
   position.y = 0;
   int r = 200;
 
   printf("ok\n");
-  SDL_Surface *Main = SDL_CreateRGBSurface(SDL_HWSURFACE,640,480,32,0,0,0,0);
-  defPixel(Main, zerox, zeroy, SDL_MapRGB(Main->format,255,255,255));
+  SDL_Surface *Main = IMG_Load("fond.jpg");//SDL_CreateRGBSurface(SDL_HWSURFACE,X,Y,32,0,0,0,0);
+  defPixel(Main, zerox, zeroy, SDL_MapRGB(Main->format,rd,gd,bd));
+  drawModulo(Main,mod, r);
 
-  while(table<20)
+
+  while(table<max)
   {
-//texte = TTF_RenderText_Blended(police, "Salut les Zr0s !", couleurNoire); 
-  printf("ok\n");
- // texte = TTF_RenderText_Shaded(police, mod, couleurNoire, couleurNoire1);
+    //table++; 
+    //texte = TTF_RenderText_Blended(police, "Salut les Zr0s !", couleurNoire); 
+    //printf("ok\n");
+    // texte = TTF_RenderText_Shaded(police, mod, couleurNoire, couleurNoire1);
     /*dessine le cercle*/////////////////  
-
+    printf("Table de %f modulo %d\n", table, mod);
     for (int i = zerox-r; i<= zerox+r; i++)
       for(int j = zeroy-r; j <= zeroy+r; j++)
-        if((pow1(i-zerox) + pow1(j-zeroy) > pow1(r)) && (pow1(i-zerox) + pow1(j-zeroy) < pow1(r+1))  )
-          defPixel(Main, i, j, SDL_MapRGB(Main->format,255,255,255));
+        if( (pow1(i-zerox) + pow1(j-zeroy) <= pow1(r+1)) && (pow1(i-zerox) + pow1(j-zeroy) >= pow1(r)) )
+          defPixel(Main, i, j, SDL_MapRGB(Main->format,rd,gd,bd));
 
 
-    float size;
-    printf("taille: %f\n", size = sizeCircle(r)); 
+    //float size;
+    //printf("taille: %f\n", size = sizeCircle(r)); 
 
-    drawModulo(Main,mod , r);
     findEq(Main, zerox, zeroy,  r, table);
 
-    for(int i  = 0; i<mod; i++)
-      printf("x: %d, y: %d, nb: %d\n", tab[i]->x, tab[i]->y, tab[i]->nb);
+    //for(int i  = 0; i<mod; i++)
+    //printf("x: %d, y: %d, nb: %d\n", tab[i]->x, tab[i]->y, tab[i]->nb);
 
     SDL_BlitSurface(Main, NULL, ecran, &position);
 
     SDL_Flip(ecran);
-    SDL_FillRect(Main, NULL, SDL_MapRGB(ecran->format, 0, 0, 0)); 
-   sleep(0.2);
-  table+=0.1; 
+    Main = IMG_Load("fond.jpg");//SDL_FillRect(Main, NULL, SDL_MapRGB(ecran->format, rf, gf, bf)); 
+    //sleep(0.2);
+    table+=v; 
+    //mod += vmod;
   }
   pause1();
-  TTF_CloseFont(police);
-  SDL_Quit();
-TTF_Quit();
+  //TTF_CloseFont(police);
+  //SDL_Quit();
+  //TTF_Quit();
   return EXIT_SUCCESS; 
 }
 
-
+int poww(int a, int b)
+{
+  if (b == 1)
+    return 1;
+  else
+    return (a%mod)*(poww(a, b-1)%mod);
+}
 void findEq(SDL_Surface *s, int zerox, int zeroy, int r, float t)
 {
-
-  struct vector *a, *b;
-
   for(int i = 0; i<mod; i++)
   {
     struct vector *a, *b;
     a = tab[i];
-    b = findVal((int)(i*t)%mod);
-    printf("find: %d\n", b->nb);
-    drag(s, zerox, zeroy, r, a->x, a->y, b->x, b->y);
+    if(*type == 'm')
+      b = findVal((int)(i*t)%mod);
+    else if(*type == 'a')
+      b = findVal((int)(i+t)%mod);
+    else if(*type == 'p')
+      b = findVal((int)(pow(i, t/10))%mod);
+    else if(*type == 's')
+      b= findVal((int)sqrt(i*t)%mod);
+    else if(*type == 'e')
+      b= findVal((int)exp(i*t/10)%mod);
+    else if(*type == 'f')
+      b= findVal((int)(t+10)%mod);
+    else if(*type == 'n')
+      b= findVal((int)log(i*t*10+1)%mod);
+    else if(*type == 'c')
+      b= findVal((int)(i*cos(t))%mod);
+    else if(*type == 'i')
+      b= findVal((int)(i*sin(t))%mod);
+
+
+
+    if(a->nb != b->nb)
+    {
+      drag(s, zerox, zeroy, r, a->x, a->y, b->x, b->y);
+
+    }
+
+
+
+    //printf("find: %d\n", b->nb);
   }
 }
 
@@ -111,7 +158,7 @@ struct vector* findVal(int v)
   {
     i++;
   }
-  printf("find: %d\n", tab[i]->nb);
+  //printf("find: %d\n", tab[i]->nb);
   return tab[i];
 }
 
@@ -126,24 +173,25 @@ void drawModulo(SDL_Surface *Main, int n, int r)
   v->nb = 0;
   tab[0] = v;
   nb ++;
-  for(int i = 1; i<n; i++)
+  for(int i = 1; i<n; i++)//iciiiiiiiiiiiiiiiiiiiiiiiiiiiii
   {
     struct vector *l = malloc(sizeof(struct vector));
     l = rotate(tab[nb-1], mod, r);
     l->nb = i;
-    tab[i] = l;
     nb++;
+    tab[i] = l;//nb par i
+
   }
-  for (int i = 0; i<n; i++)
+  for(int i = 0; i<n; i++)//nb par n
   {
-    tab[i]->x+=640/2;
-    tab[i]->y+=480/2;
-
-    defPixel(Main, tab[i]->x, tab[i]->y, SDL_MapRGB(Main->format, 250, i*50, i*50));
-    defPixel(Main, tab[i]->x-1, tab[i]->y, SDL_MapRGB(Main->format, 250, i*50, i*50));
-    defPixel(Main, tab[i]->x, tab[i]->y-1, SDL_MapRGB(Main->format, 250, i*50, i*50));
-    defPixel(Main, tab[i]->x-1, tab[i]->y-1, SDL_MapRGB(Main->format, 250, i*50, i*50));
-
+    tab[i]->x+=X/2;
+    tab[i]->y+=Y/2;
+    /*
+       defPixel(Main, tab[i]->x, tab[i]->y, SDL_MapRGB(Main->format, 250, i*50, i*50));
+       defPixel(Main, tab[i]->x-1, tab[i]->y, SDL_MapRGB(Main->format, 250, i*50, i*50));
+       defPixel(Main, tab[i]->x, tab[i]->y-1, SDL_MapRGB(Main->format, 250, i*50, i*50));
+       defPixel(Main, tab[i]->x-1, tab[i]->y-1, SDL_MapRGB(Main->format, 250, i*50, i*50));
+       */
   }
 
 }
@@ -167,14 +215,15 @@ struct vector* rotate(struct vector *a, int n, int r)
 void drag(SDL_Surface *s,int zerox, int zeroy,int r, float ax, float ay, float bx, float by)
 {
   float coef = (by-ay)/(bx-ax);
-  printf("coef dir: %f\n", coef);
+  //printf("coef dir: %f\n", coef);
   float b = (ay-coef*ax);
   for (int i = zerox-r; i< zerox+r; i++)
     for(int j = zeroy-r; j < zeroy+r; j++)
     {
+
       if(j >= (int)(coef*(i)+b) && j <= (int)(coef*(i)+1+b) && (pow1(i-zerox) + pow1(j-zeroy) < pow1(r)) )
       {
-        defPixel(s, i, j, SDL_MapRGB(s->format,255,255,255));
+        defPixel(s, i, j, SDL_MapRGB(s->format,rd,gd,bd));
 
         //printf("ok");
       }
@@ -192,6 +241,13 @@ float sizeCircle(int r)
 float pow1(int a)
 {
   return a*a;
+}
+float pow2(float a, int b)
+{
+  if(b == 1)
+    return 1;
+  else
+    return a*(pow2(a, b-1));
 }
 
 
@@ -258,14 +314,14 @@ void pause1()
   while (continuer)
   {
 
-    
-       SDL_WaitEvent(&event);
-       switch(event.type)
-       {
-       case SDL_QUIT:
-       continuer = 0;
-       }
-       
-   // mod++;
+
+    SDL_WaitEvent(&event);
+    switch(event.type)
+    {
+      case SDL_QUIT:
+        continuer = 0;
+    }
+
+    // mod++;
   }
 }
